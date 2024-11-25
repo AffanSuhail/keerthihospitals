5027d8959b70d3e53a38fab67319ca4abf075bef
AffanSuhail <43168999+AffanSuhail@users.noreply.github.com>
2024-11-25 11:33:00 +0530

    Update send.php

--- send.php
+++ send.php
@@ -21,7 +21,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "info@keerthihospitals.com";
+        $recipient = "nilofer.akb@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
